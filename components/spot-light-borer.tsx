1900995d87c1fe9f53f64e2a9aca9bb2a962248a
Mukhammadxuja Anvarov <87266381+mukhammadxuja@users.noreply.github.com>
2024-03-29 12:49:32 +0500

    feat: add input (not fully ready)

---
+++ components/spot-light-borer.tsx
@@ -0,0 +1,63 @@
+import React, { useRef, useState } from "react";
+
+export const SpotlightBorder = () => {
+  const divRef = useRef<HTMLInputElement>(null);
+  const [isFocused, setIsFocused] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [opacity, setOpacity] = useState(0);
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLInputElement>) => {
+    if (!divRef.current || isFocused) return;
+
+    const div = divRef.current;
+    const rect = div.getBoundingClientRect();
+
+    setPosition({ x: e.clientX - rect.left, y: e.clientY - rect.top });
+  };
+
+  const handleFocus = () => {
+    setIsFocused(true);
+    setOpacity(1);
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+    setOpacity(0);
+  };
+
+  const handleMouseEnter = () => {
+    setOpacity(1);
+  };
+
+  const handleMouseLeave = () => {
+    setOpacity(0);
+  };
+
+  return (
+    <div className="input-wrapper relative">
+      <input
+        onMouseMove={handleMouseMove}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        autoComplete="off"
+        placeholder="Enter your email address"
+        type="email"
+        name="email"
+        className="base-input"
+      />
+      <input
+        ref={divRef}
+        disabled
+        style={{
+          border: "1px solid rgb(228, 115, 32)",
+          opacity,
+          WebkitMaskImage: `radial-gradient(30% 30px at ${position.x}px ${position.y}px, black 45%, transparent)`,
+        }}
+        aria-hidden="true"
+        className="overlay-input"
+      />
+    </div>
+  );
+};
