37d2f694728d33138e0e5512c1f3c2ce45f2dab0
Mukhammadxuja Anvarov <87266381+mukhammadxuja@users.noreply.github.com>
2024-04-02 20:33:27 +0500

    update: button variants

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -4,22 +4,29 @@ import { cn } from "@/utils/cn  ";
 
 type ButtonPops = React.HTMLAttributes<HTMLButtonElement> & {
   variant?: "primary" | "secondary" | "ghost" | "outline";
+  size?: "sm" | "md" | "lg";
+  radius?: "full" | "md" | "none";
   icon?: boolean;
 };
 
 export default function Button({
   className,
   variant,
+  radius,
+  size,
   icon = false,
   ...props
 }: ButtonPops): JSX.Element {
   return (
-    <button {...props} className={cn(buttonVariants({ variant }), className)} />
+    <button
+      {...props}
+      className={cn(buttonVariants({ variant, radius, size }), className)}
+    />
   );
 }
 
 const buttonVariants = cva(
-  "w-full md:w-fit rounded-[8px] px-[16px] py-[7px] text-center text-[16px] tracking-[-0.1px] leading-[1.3em] font-semibold duration-300 hover:bg-opacity-80 hover-shadow-md active:translate-y-0.5 disabled:opacity-20 disabled:cursor-not-allowed shadow",
+  "w-full md:w-fit text-center tracking-[-0.1px] leading-[1.3em] font-semibold duration-300 hover:bg-opacity-80 hover-shadow-md active:translate-y-0.5 disabled:opacity-20 disabled:cursor-not-allowed shadow",
   {
     variants: {
       variant: {
@@ -31,9 +38,21 @@ const buttonVariants = cva(
         outline:
           "bg-transparent hover:bg-primary hover:text-white border border-primary dark:border-secondary dark:hover:bg-secondary dark:hover:text-black",
       },
+      size: {
+        sm: "px-[13px] py-[5px] text-[13px]",
+        md: "px-[16px] py-[7px] text-[16px]",
+        lg: "px-[20px] py-[12px] text-[16px]",
+      },
+      radius: {
+        full: "rounded-full after:rounded-full",
+        md: "rounded-[8px] after:rounded-[8px]",
+        none: "rounded-[3px] after:rounded-[3px]",
+      },
     },
     defaultVariants: {
       variant: "primary",
+      radius: "md",
+      size: "md",
     },
   }
 );
